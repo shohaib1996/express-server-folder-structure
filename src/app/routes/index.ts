00487bce770262e465127c433e55d571048d707e
shohaib1996 <khanshohaibhossain@gmail.com>
2024-06-11 11:52:03 +0600

    Module 16 complete

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,8 @@ import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academic
 import { FacultyRoutes } from "../modules/faculty/faculty.route";
 import { AdminRoutes } from "../modules/admin/admin.route";
 import { CourseRoutes } from "../modules/course/course.route";
+import { semesterRegistrationRoutes } from "../modules/semesterRegistration/semesterRegistration.route";
+import { offeredCourseRoutes } from "../modules/offeredCourse/offeredCourse.route";
 
 const router = Router();
 
@@ -42,6 +44,14 @@ const moduleRoutes = [
   {
     path: "/courses",
     route: CourseRoutes,
+  },
+  {
+    path: "/semester-registrations",
+    route: semesterRegistrationRoutes,
+  },
+  {
+    path: "/offered-courses",
+    route: offeredCourseRoutes,
   },
 ];
 
